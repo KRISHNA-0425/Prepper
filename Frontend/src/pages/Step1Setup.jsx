6259b7c3a81c072c07320595f837f29dfcd25a7f
Krishna Sinha <sinhakishu0425@gmail.com>
2026-03-18 22:00:10 +0530

    Update Step1Setup.jsx

--- Frontend/src/pages/Step1Setup.jsx
+++ Frontend/src/pages/Step1Setup.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Step1Setup({ onStart }) {
     return (
         <div className='text-lg font-bold ' > 
-            {onStart}
+            
         </div>
     )
 }
